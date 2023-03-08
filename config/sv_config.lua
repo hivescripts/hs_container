e2f6dca553ea3999b64224cdb5880d05f4b0b2e4
DerDevHD <59358798+DerDevHD@users.noreply.github.com>
2023-03-08 15:36:16 +0100

    removed old testing webhooks, changed description of ESX

--- config/sv_config.lua
+++ config/sv_config.lua
@@ -1,8 +1,8 @@
 ServerConfig = {}
 
-ServerConfig.ESXType = true -- true = using export (ESX Legacy), false = using callback (ESX 1.1, etc.)
+ServerConfig.ESXType = true -- true = using export (ESX Legacy), false = using callback (old ESX)
 
-ServerConfig.CommunityWebhook = 'https://discord.com/api/webhooks/1072535708750860338/PEr_kdoCmrrxcs-USvb-kc8HeYTWoJmJaNEEIM6o9tD158JViJsI028bIxxTqY7DpN6C'
+ServerConfig.CommunityWebhook = ''
 
-ServerConfig.Webhook = 'https://discord.com/api/webhooks/1072535708750860338/PEr_kdoCmrrxcs-USvb-kc8HeYTWoJmJaNEEIM6o9tD158JViJsI028bIxxTqY7DpN6C'
+ServerConfig.Webhook = ''
 ServerConfig.LogIPs = false
